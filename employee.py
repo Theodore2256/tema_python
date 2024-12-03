5541571f82bcea8507eb9983ff3bdb162b4e321e
Stefan Alexandru Ionescu <stefan@Stefans-MacBook-Air-3.local>
2024-12-03 22:28:50 +0200

    Done

--- employee.py
+++ employee.py
@@ -13,7 +13,7 @@ class Employee:
         print(f"Total number of employee(s) is {Employee.emp_count}")
 
     def display_employee(self):
-        print("Name : ", self.name, ", Salary: ", self.salary)
+        print("Salary: ", self.salary)
 
     def __del__ (self):
         Employee.empCount -=1
@@ -23,20 +23,36 @@ class Employee:
         self.salary = new_salary
 
 
-class Employee : Manager:
-    def __init__ (self ,name,salary)
-        
-##    def add_task(self, task_name):
-##        self.tasks[task_name] = "New"   # needs tasks defined before (in __init__)
-##
-##    def update_tasks(self, task_name, status):
-##        self.tasks[task_name] = status
-    def modify_task(self, task_name, status="New"):
-        self.tasks[task_name]=status
+class Manager(Employee):
+    """Manager class inheriting from Employee"""
+    mgr_count = 0
 
-    def display_task(self, status):
-        print(f"Taskuri cu statusul {status}")
-        for name in self.tasks.keys():
-            if self.tasks[name] == status:
-                print(name)
+    def __init__(self, name, salary, department):
+        super().__init__(name, salary)
+        self.department = f"Echipa {department}"
+        Manager.mgr_count += 1
+ # Creating Employee objects
+empl1 = Employee("Vlad Andrei", 6000)
+empl2 = Employee("Apostu George", 8500)
+empl3 = Employee("Ionescu Andrei", 9000)
+empl4 = Employee("Niculescu Mihaela ", 400000)
+empl5 = Employee("Popescu Maria", 1000000)
+
+    
+# Creating Manager objects
+manager1 = Manager("Ionescu Andrei", 70000, "IT")
+manager2 = Manager("Pop Nicolaie", 3500, "Human Resources")
+manager3 = Manager("Badea Vlad", 7000, "Informatica")
+manager4 = Manager("Alexe Gabriela ", 40000, "Human Resources")
+manager5 = Manager("Baciu Andreea", 650000, "IT")
+
+# Salary Out
+manager1.display_employee()
+empl2.display_employee()
+
+# Nr of elements
+print(f"Number of Employee objects: {Employee.empCount}")
+print(f"Number of Manager objects: {Manager.mgr_count}")
+
+        
 
