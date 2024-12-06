b98c53df53e4fc6ce413f4f7144537d32c628413
Stefan Alexandru Ionescu <stefan@Stefans-MacBook-Air-3.local>
2024-12-07 00:01:52 +0200

    test3

--- Stefan/README.md
+++
@@ -1,4 +0,0 @@
-#tema_Pytohn
-# tema_python
-1 Am adaugat constructorul clasei Manager  si counter ul de manageni 
-
--- ex2.py
+++ ex2.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('data.csv')
 plt.figure(figsize=(12, 6))
 for column in df.columns:
-    plt.plot(df[column], label=column)
+  plt.plot(df[column], label=column)
 plt.show()
 
 
