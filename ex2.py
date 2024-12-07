ca8264db8b485d61375ab891d0565f8a5dde2c23
Stefan Alexandru Ionescu <stefan@Stefans-MacBook-Air-3.local>
2024-12-07 13:42:23 +0200

    final form

--- ex2.py
+++ ex2.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
+nume="ionescu"
+prenume="stefan alexandru"
+x=len(nume)
+y=len(prenume)
 
 df = pd.read_csv('data.csv')
-plt.figure(figsize=(12, 6))
-for column in df.columns:
-  plt.plot(df[column], label=column)
+df_x = df.head(x)  #x=7
+df_y= df[['Durata', 'Puls']].head(y) #y=15
+df.plot()
+df_x.plot()
+df_y.plot()
 plt.show()
 
 
